471850306e54acfcf52ad16dede8756b9ec29cb1
sijan67 <sijuichi1@gmail.com>
2024-10-02 15:44:11 -0700

    chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,284 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @jsxRuntime classic
+ * @jsx jsx
+ */
+import { type ReactNode, ReactElement } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
+import { css, jsx } from "@emotion/react";
+import { monitorForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
+import king from "./assets/king.png";
+import pawn from "./assets/pawn.png";
+import invariant from "tiny-invariant";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
+export type Coord = [number, number];
+
+export type PieceType = "king" | "pawn";
+
+export type PieceRecord = {
+  type: PieceType;
+  location: Coord;
+};
+
+type PieceProps = {
+  location: Coord;
+  pieceType: PieceType;
+  image: string;
+  alt: string;
+};
+
+export function isEqualCoord(c1: Coord, c2: Coord): boolean {
+  return c1[0] === c2[0] && c1[1] === c2[1];
+}
+
+export const pieceLookup: {
+  [Key in PieceType]: (location: [number, number]) => ReactElement;
+} = {
+  king: (location) => <King location={location} />,
+  pawn: (location) => <Pawn location={location} />,
+};
+
+export function canMove(
+  start: Coord,
+  destination: Coord,
+  pieceType: PieceType,
+  pieces: PieceRecord[]
+) {
+  const rowDist = Math.abs(start[0] - destination[0]);
+  const colDist = Math.abs(start[1] - destination[1]);
+
+  if (pieces.find((piece) => isEqualCoord(piece.location, destination))) {
+    return false;
+  }
+
+  switch (pieceType) {
+    case "king":
+      return [0, 1].includes(rowDist) && [0, 1].includes(colDist);
+    case "pawn":
+      return colDist === 0 && start[0] - destination[0] === -1;
+    default:
+      return false;
+  }
+}
+
+interface SquareProps {
+  pieces: PieceRecord[];
+  location: Coord;
+  children: ReactNode;
+}
+
+function getColor(state: HoveredState, isDark: boolean): string {
+  if (state === "validMove") {
+    return "lightgreen";
+  } else if (state === "invalidMove") {
+    return "pink";
+  }
+  return isDark ? "lightgrey" : "white";
+}
+
+type HoveredState = "idle" | "validMove" | "invalidMove";
+
+export function isCoord(token: unknown): token is Coord {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Array.isArray(token) &&
+    token.length === 2 &&
+    token.every((val) => typeof val === "number")
+  );
+}
+
+const pieceTypes: PieceType[] = ["king", "pawn"];
+
+export function isPieceType(value: unknown): value is PieceType {
+  return typeof value === "string" && pieceTypes.includes(value as PieceType);
+}
+
+function Square({ pieces, location, children }: SquareProps) {
+  const ref = useRef(null);
+  const [state, setState] = useState<HoveredState>("idle");
+
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+
+    return dropTargetForElements({
+      element: el,
+      getData: () => ({ location }),
+      canDrop: ({ source }) => {
+        if (!isCoord(source.data.location)) {
+          return false;
+        }
+
+        return !isEqualCoord(source.data.location, location);
+      },
+      onDragEnter: ({ source }) => {
+        if (
+          !isCoord(source.data.location) ||
+          !isPieceType(source.data.pieceType)
+        ) {
+          return;
+        }
+
+        if (
+          canMove(source.data.location, location, source.data.pieceType, pieces)
+        ) {
+          setState("validMove");
+        } else {
+          setState("invalidMove");
+        }
+      },
+      onDragLeave: () => setState("idle"),
+      onDrop: () => setState("idle"),
+    });
+  }, [location, pieces]);
+
+  const isDark = (location[0] + location[1]) % 2 === 1;
+
+  return (
+    <div
+      css={squareStyles}
+      style={{ backgroundColor: getColor(state, isDark) }}
+      ref={ref}
+    >
+      {children}
     </div>
   );
 }
 
-export default App;
+function renderSquares(pieces: PieceRecord[]) {
+  const squares = [];
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const squareCoord: Coord = [row, col];
+
+      const piece = pieces.find((piece) =>
+        isEqualCoord(piece.location, squareCoord)
+      );
+
+      squares.push(
+        <Square pieces={pieces} location={squareCoord}>
+          {piece && pieceLookup[piece.type](squareCoord)}
+        </Square>
+      );
+    }
+  }
+  return squares;
+}
+
+function Chessboard() {
+  const [pieces, setPieces] = useState<PieceRecord[]>([
+    { type: "king", location: [3, 2] },
+    { type: "pawn", location: [1, 6] },
+  ]);
+
+  useEffect(() => {
+    return monitorForElements({
+      onDrop({ source, location }) {
+        const destination = location.current.dropTargets[0];
+        if (!destination) {
+          return;
+        }
+        const destinationLocation = destination.data.location;
+        const sourceLocation = source.data.location;
+        const pieceType = source.data.pieceType;
+
+        if (
+          !isCoord(destinationLocation) ||
+          !isCoord(sourceLocation) ||
+          !isPieceType(pieceType)
+        ) {
+          return;
+        }
+
+        const piece = pieces.find((p) =>
+          isEqualCoord(p.location, sourceLocation)
+        );
+        const restOfPieces = pieces.filter((p) => p !== piece);
+
+        if (
+          canMove(sourceLocation, destinationLocation, pieceType, pieces) &&
+          piece !== undefined
+        ) {
+          setPieces([
+            { type: piece.type, location: destinationLocation },
+            ...restOfPieces,
+          ]);
+        }
+      },
+    });
+  }, [pieces]);
+
+  return <div css={chessboardStyles}>{renderSquares(pieces)}</div>;
+}
+
+const Piece = ({ location, pieceType, image, alt }: PieceProps) => {
+  const ref = useRef(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+
+    return draggable({
+      element: el,
+      getInitialData: () => ({ location, pieceType }),
+      onDragStart: () => setDragging(true),
+      onDrop: () => setDragging(false),
+    });
+  }, [location, pieceType]);
+
+  return (
+    <img
+      css={imageStyles}
+      style={dragging ? { opacity: 0.4 } : {}} // fading the piece during dragging
+      src={image}
+      alt={alt}
+      ref={ref}
+    />
+  );
+};
+
+export function King({ location }: { location: [number, number] }) {
+  return (
+    <Piece location={location} pieceType={"king"} image={king} alt="King" />
+  );
+}
+
+export function Pawn({ location }: { location: [number, number] }) {
+  return (
+    <Piece location={location} pieceType={"pawn"} image={pawn} alt="Pawn" />
+  );
+}
+
+const chessboardStyles = css({
+  display: "grid",
+  gridTemplateColumns: "repeat(8, 1fr)",
+  gridTemplateRows: "repeat(8, 1fr)",
+  width: "500px",
+  height: "500px",
+  border: "3px solid lightgrey",
+});
+
+const squareStyles = css({
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const imageStyles = css({
+  width: 45,
+  height: 45,
+  padding: 4,
+  borderRadius: 6,
+  boxShadow:
+    "1px 3px 3px rgba(9, 30, 66, 0.25),0px 0px 1px rgba(9, 30, 66, 0.31)",
+  "&:hover": {
+    backgroundColor: "rgba(168, 168, 168, 0.25)",
+  },
+});
+
+export default Chessboard;
